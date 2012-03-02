86d170efb5c55d2f677530665080526f546c9171
Erik Larson <erik@pickgrapevine.com>
2012-03-01 21:29:24 -0600

    Ending at 6.2.3 Validation

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,37 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
+require 'spec_helper'
+
+describe User do
+  
+  before do
+	  @user = User.new(name: "Example User", email: "user@example.com")
+	end
+  
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+
+  it { should be_valid }
+
+  describe "when name is not present" do
+  	before { @user.name = " " }
+  	it { should_not be_valid }
+  end
+
+  describe "when email is not present" do
+  	before { @user.email = " " }
+  	it { should_not be_valid }
+  end
+end
